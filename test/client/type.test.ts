f7e01a24667b2be6e3ed8cf92ec2a81b72d3a4c9
Eaviyi <eavidy@qq.com>
2025-12-07 02:58:41 +0800

    fix:

--- test/client/type.test.ts
+++ test/client/type.test.ts
@@ -1,6 +1,8 @@
 import { InferQueryResult, SqlStatementDataset, SqlTextStatementDataset } from "@asla/yoursql";
-import { expectTypeOf } from "vitest";
+import { expectTypeOf, test } from "vitest";
 
-expectTypeOf<InferQueryResult<SqlStatementDataset<{ age: number }>>>().toEqualTypeOf<{ age: number }>();
+test("推断类型", function () {
+  expectTypeOf<InferQueryResult<SqlStatementDataset<{ age: number }>>>().toEqualTypeOf<{ age: number }>();
 
-expectTypeOf<InferQueryResult<SqlTextStatementDataset<{ age: number }>>>().toEqualTypeOf<{ age: number }>();
+  expectTypeOf<InferQueryResult<SqlTextStatementDataset<{ age: number }>>>().toEqualTypeOf<{ age: number }>();
+});
